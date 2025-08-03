360a78df03acfdf32cae8bb0277f62e5cf7177f8
Amar <amaratarking@gmail.com>
2025-08-04 00:15:20 +0300

    1

---
+++ main.py
@@ -0,0 +1,24 @@
+print("hello")
+#print ()للطباعة
+
+# (#)للكومنت
+
+# انواع البيانات
+#لايوجد حرف واحد 
+print("______________")
+
+print(type("hello"))
+#str = string نص
+print(type(10))
+#int = intger رقم صحيح
+print(type(10.1))
+#float = float رقم عشري
+print(type([1, 2, 3, 4, 5,]))
+#list = list قاءمة تعدل []
+print(type((1, 2, 3, 4, 5,)))
+#tuple = tuple قاءمة ثوابت()
+print(type({"amar": "atar", "obada":"atar"}))
+#dict = dintionary وفيه تكتب قيمة  وشي يساويها؟
+print(type(2 == 2)) #True = 1
+print(type(2 == 4)) #false = 10
+#bool = boolean نوع بيانات يخزنة قيمة صح اوخطء 
