50ae97ea5addae184b2dadcee3b42de0eb1cdc83
Amar <amaratarking@gmail.com>
2025-08-26 21:04:50 +0300

    Add files via upload

---
+++ 8.py
@@ -0,0 +1,50 @@
+print("___number___") 
+
+#intger = int
+print(type(3))
+
+#float = float
+print(type(100.0993))
+
+# complax نوع ارقام حقيقية وليسة حقيقية شي رياضي افهمها لان تفهم بمثال
+a1 = 7+8j
+print(type(a1))
+print("real part is {}".format (a1.real))
+print("imaginary is {}".format (a1.imag))
+
+#1. int ---->float , complex
+#2. float ----> int , complex
+#3. complax ---> لايمكن
+
+print(100)
+print(float(100))
+print(complex(100))
+
+print(100,50)
+print(int(100.50))
+print(complex(100.50))
+
+#print(7+8j)
+#print(float(7+8j))
+#print(int(7+8j))
+ 
+#+
+print(10 + 10)
+#-
+print(10 - 30)
+#* ضرب
+print(110 * 59425)
+print (5+4 * 11)
+print ((5+4) * 11)
+#/ قسمة
+print(100 / 20)
+print(int(100 / 20))
+#باقي القسمة %
+print(9 % 2)
+#** لاس 
+print(2 ** 5)
+print(2 * 2 * 2 * 2* 2)
+# / / عملية القسمة لاكن بدون كسور تاخذ الرقم الذي تحتها حتا لو كان سالب ليطلع رقم صحيح تفهم بمثال
+print(100 // 20) # 5
+print(110 // 20) # 5 
+print(120 // 20) # 6
